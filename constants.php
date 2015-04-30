30719a667c7684c80b68ef6d4fc382381a3913f5
jdia07 <jdia07@users.noreply.github.com>
2015-04-30 13:03:16 +0800

    Create constants.php

---
+++ constants.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author jdia07 
+ * @copyright (c) 2015, Vainglory07 02.07
+ * 
+ */
+
+define('PROJECT_NAME', ''); // Will show as title on views
+define('PROJECT_PREF', ''); // Default Prefix for css|js class'/id's init
+
+// Datetime
+define('TIMEZONE',          'Asia/Manila');
+define('LOG_DATE',          'Ymd');
+define('LOG_TIME',          'H:i:s');
+define('DATETIME_FORMAT',   'd/m/Y H:i:s');	// System DateTime Format
+define('TIMESTAMP_FORMAT',  'Y-m-d H:i:s');	// Default Timestamp Format
+define('DATETIME_EMPTY',    '0000-00-00 00:00:00');
