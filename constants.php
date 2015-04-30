7bca35e5badd4f89d3aa903851b238bfafe69fdd
jdia07 <jdia07@users.noreply.github.com>
2015-04-30 13:42:23 +0800

    Update constants.php

--- constants.php
+++ constants.php
@@ -6,12 +6,12 @@
  */
 
 define('PROJECT_NAME', ''); // Will show as title on views
-define('PROJECT_PREF', ''); // Default Prefix for css|js class'/id's init
+define('PROJECT_PREF', ''); // Default Prefix for css|js classes|ids'
 
 // Datetime
 define('TIMEZONE',          'Asia/Manila');
 define('LOG_DATE',          'Ymd');
 define('LOG_TIME',          'H:i:s');
 define('DATETIME_FORMAT',   'd/m/Y H:i:s');	// System DateTime Format
-define('TIMESTAMP_FORMAT',  'Y-m-d H:i:s');	// Default Timestamp Format
+define('TIMESTAMP_FORMAT',  'Y-m-d H:i:s A');	// Default Timestamp Format
 define('DATETIME_EMPTY',    '0000-00-00 00:00:00');
